92431d012365da14ea382f03694a8c9a4d265c9c
tetomelo <teto04@gmail.com>
2025-09-04 21:52:58 -0300

    javascript04/09

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-alert("Olá mundo")
+// alert("Olá mundo")
+// alert("Oi")
+// alert("Tudo bem?")
+// alert("Você vem sempre aqui?")
+
+// let idade 
+// idade = 18 
+// alert(idade)
+// idade = "dezoito"
+// alert(idade)
+
+// idade = "melancia"
+// alert(idade)
+
+// idade = 18.1
+// alert(idade)
+
+const amor = "Meu primeiro amor"
+alert(amor)
+
+let amor2 = "Meu segundo amor"
+alert(amor2)
+
+var antigao = "Nunca usar essa poioca"
+alert(antigao)
