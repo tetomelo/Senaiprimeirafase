65b6838470259674da58c87ec8d7cdab3e61f2e9
tetomelo <teto04@gmail.com>
2025-09-09 22:15:32 -0300

    variaveis javascript

---
+++ logica-js/variaveis.js
@@ -0,0 +1,24 @@
+// vaiavel com var
+// var nome = "Everton"
+// var nome = "Manuela"
+// nome = "Manuela"
+
+//variavel com let
+// let nome = "Everton"
+// nome = "Manuela" 
+
+//variavel declarada com const 
+const nome = "Everton" //string
+const sobrenome = "Ferreira" 
+const idade = 19 //number
+const masculino = true //boolean
+
+//array
+const frutas = ["laranja", "maçã", "banana"]
+
+console.log(frutas[0])
+
+
+// console.log("Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade + " anos")
+
+// console.log(`Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`)
