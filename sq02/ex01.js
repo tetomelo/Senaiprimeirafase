dc108a665f1b129a9200c03ef8b68b6c8302b21a
tetomelo <teto04@gmail.com>
2025-09-10 22:18:58 -0300

    sq02

---
+++ sq02/ex01.js
@@ -0,0 +1,17 @@
+let idade 
+
+
+idade = Number(prompt("Digite sua idade: "))
+
+if(idade >= 18){
+    alert("Maior de idade")
+    alert("Pode sair pro intervalo")
+  }else{
+    alert("Dimenó")
+    alert("Pode sair pro recreio")
+  }
+
+
+
+
+console.log(idade);
---
+++ sq02/ex04.js
@@ -0,0 +1,16 @@
+let numero
+numero = Number(prompt("Digite o número: "))
+if(numero % 2 == 0){
+    alert("par")
+}else{
+    alert("impar")
+}
+
+
+
+// ler um número 
+
+// se o resto da divisão por 2 for 0 // o numero for par
+//     informar que é par
+// senão
+//     informar que é impar 
