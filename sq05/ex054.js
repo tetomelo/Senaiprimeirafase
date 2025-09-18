0b0bb10eb544a1be6bc3257d747b072cceebc0f1
tetomelo <teto04@gmail.com>
2025-09-17 21:58:24 -0300

    sq05

---
+++ sq05/ex054.js
@@ -0,0 +1,14 @@
+let palestra
+
+palestra = Number(prompt("Digite o número da palestra: "))
+if(palestra == 1){
+    alert("Animação com Scratch, laboratório 305, 19h")
+}else if(palestra == 2){
+    alert("Scratch para gamers, laboratório 512, 20h")
+}else if(palestra == 3){
+    alert("JavaScript para leigos, laboratório 101, 19h")
+}else if(palestra == 4){
+    alert("Topicos avamçados de JavaScript, laboratório 305, 20h")
+}else if(palestra == 5){
+    alert("Vida e carreira, auditório, 21h")
+}
