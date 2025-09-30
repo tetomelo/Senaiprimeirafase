a6d2aa8196f82c6e75880eb71fdf25f3a0b9d21b
tetomelo <teto04@gmail.com>
2025-09-29 22:15:57 -0300

    Repetições

---
+++ Repetições/script.js
@@ -0,0 +1,16 @@
+// let acertos = Number(prompt("Quantos acertos?"))
+// let valorPorQuestao = 10/16
+// let nota = acertos * valorPorQuestao
+
+// alert("Sua nota: " + nota)
+
+
+
+let contador = 0
+while(contador < 2){
+    // alert("contador: " + contador)
+     console.log("Contador: " + contador)
+    contador = contador + 1 
+}
+
+
