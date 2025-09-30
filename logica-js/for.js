b7eecf35c58cdc9bf95c602f3b5e8667f656bc18
tetomelo <teto04@gmail.com>
2025-09-30 20:30:23 -0300

    logica-js

---
+++ logica-js/for.js
@@ -0,0 +1,24 @@
+// for(let i = 0; i<=10; i++){
+    // console.log(i)
+// }
+
+// for(let i = 10; i>=0; i--){
+    // console.log(i)
+// }
+
+// for(let i = 0; i<=20; i++){
+//     console.log(i)
+// }
+
+// let soma = 0
+// for(let i = 1; i<100; i++){
+//     soma += i
+// }
+
+// console.log("Resultado da soma", soma)
+
+
+
+let alunos = ["João","Maria","Joana"]
+alunos.push("Pedro")
+console.log(alunos.length)
