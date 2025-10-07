6eb0d4fb11bec79f6a22883314f0acff6075219b
tetomelo <teto04@gmail.com>
2025-10-06 22:00:27 -0300

    Repetições

--- Repetições/for.js
+++ Repetições/for.js
@@ -29,10 +29,10 @@
 
 
 //01     
-    for(let numero = 1; numero <= 10; numero ++){
-        let quadrado = numero * numero
-          alert(`O quadrado de ${numero} é ${quadrado}`)
-    }
+    // for(let numero = 1; numero <= 10; numero ++){
+    //     let quadrado = numero * numero
+    //       alert(`O quadrado de ${numero} é ${quadrado}`)
+    // }
 
 
 
@@ -40,11 +40,55 @@
 
 
 //02
-    // for(let i = 10; i>=1; i--){
+// for(let i = 10; i>=1; i--){
     //        console.log(i)
     // }
     
+    
+//03
+    // let soma = 0
+    // console.log(soma);
+    // for(let i = 1; i<=100; i++){
+    //     soma = soma + i
+    //     console.log(soma);
+    // }
+    
+
+//05
+    // for(let i=1; i <=10; i++){
+    //     let resultado = i * 5
+    //     console.log(i + "x" + 5 + "=" + resultado);
+    // }
+
+
+    // for(let i=1; i <=10; i++){
+    //     let resultado = i * 5
+    //     console.log(i + "x" + 5 + "=" + resultado);
+    // }
+
+
+    // let numero = Number(prompt("Tabuada de quê?"))
+    // for(let i=1; i<=10; i++){
+    //     let resultado = i * numero
+    //     console.log(i + "x" + numero + "=" + resultado);
+        
+    // }
 
 
 
+    //06
+        // for(let i=1; i<=20; i++){
+        //     if(1%2 == 1){
+        //         alert(1)
+        //     }
+        // }
 
+        //  for(let i=1; i<=20;  i=i+2){
+        //         alert(1)
+        //     }
+        
+
+        // for(let i=1; i<=20; i++){ 
+        //         alert(1)
+        //     }
+        
