7709097d537d6b3e1743795302b19e510c347e19
tetomelo <teto04@gmail.com>
2025-10-23 22:18:10 -0300

    DOM Revisão

---
+++ dom-manipulação/Dom-revisão/script.js
@@ -0,0 +1,15 @@
+let usuario 
+
+function cadastrarUsuario(){
+    // console.log("até aqui deu");
+    usuario = document.getElementById("inputUsuario").value
+    
+    document.getElementById("inputUsuario").value = ' '
+
+    document.getElementById("resultado").innerHTML = "Usuario cadastrardo com sucesso: " + usuario
+}
+
+
+
+
+let 
