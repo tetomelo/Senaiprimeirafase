4d66e2552a727ba007ade77422311ab0abe546e5
tetomelo <teto04@gmail.com>
2025-10-27 22:07:35 -0300

    DOM Revisão

--- dom-manipulação/Dom-revisão/script.js
+++ dom-manipulação/Dom-revisão/script.js
@@ -12,4 +12,16 @@ function cadastrarUsuario(){
 
 
 
-let 
+function calcularEx1014(){
+    let distancia = Number(document.getElementById('inputDistancia').value)
+    let combustivel = Number(document.getElementById('inputCombustivel').value)
+
+    let consumo = distancia / combustivel
+    console.log(consumo);
+
+    //document getElementById('resultadoConsumo'). innerHTML = "O consumo foi de " + consumo + "km/l"
+    document.getElementById('resultadoConsumo'). innerHTML = `O consumo foi de ${consumo}km/l`
+    
+
+    
+}
